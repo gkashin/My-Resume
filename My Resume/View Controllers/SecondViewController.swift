e34a5c6a19fc24f4c27602eb2b1b80bc5f0f79fb
George <georgii.kashin@gmail.com>
2019-04-24 08:34:26 +0300

    Added SecondViewController

---
+++ My Resume/View Controllers/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  My Resume
+//
+//  Created by Георгий Кашин on 24/04/2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
